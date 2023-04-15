184da9e4c440f26a47671d53e1733901bba688a2
smartysanjay3001 <sanjaykmr3001@gmail.com>
2023-04-15 13:57:34 +0530

    second

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -18,7 +18,7 @@ class Blogs extends Component{
                 <div className='card'>
                     <img src={animal} alt="Animals" title='Animals' />
                     <a href="https://www.mdpi.com/journal/animals" target="_blank" title="Animals"><h2>Animals</h2></a>
-                    <p>Animal article means an article made from any captive animal or wild animal, other than vermin, and includes an article or object...</p>
+                    {/* <p>Animal article means an article made from any captive animal or wild animal, other than vermin, and includes an article or object...</p> */}
                 </div>
             </div>
          
@@ -30,7 +30,7 @@ class Blogs extends Component{
                 <div className="card">
                     <img src={birds} alt="" srcset="" />
                    <a href="#" title="Birds"><h2>Birds</h2></a> 
-                   <p>Birds are very special animals that have particular characteristics which are common amongst all of them. </p>
+                   {/* <p>Birds are very special animals that have particular characteristics which are common amongst all of them, and include an article or object... </p> */}
                 </div>
                 </div>
             {/* <!-- column3 --> */}
@@ -38,7 +38,7 @@ class Blogs extends Component{
                 <div className="card">
                   <img src={forest} alt=""/>
                   <a href="#" title="Forest"> <h2>Forest</h2></a>
-                  <p>Birds are very special animals that have particular characteristics which are common amongst all of them. </p>
+                  {/* <p>Birds are very special animals that have particular characteristics which are common amongst all of them, and include an article or object...</p> */}
                 </div>
             </div>
             {/* <!-- column4 --> */}
@@ -46,7 +46,7 @@ class Blogs extends Component{
                 <div className="card">
                    <img src={moutain} alt=""/>
                   <a href="#" title="Moutain"><h2>Moutain</h2></a>
-                  <p>Birds are very special animals that have particular characteristics which are common amongst all of them. </p> 
+                  {/* <p>Birds are very special animals that have particular characteristics which are common amongst all of them. </p>  */}
                 </div>
             </div>
             {/* <!-- column 5 --> */}
@@ -54,7 +54,7 @@ class Blogs extends Component{
                 <div className="card">
                    <img src={night} alt=""/>
                    <a href='#' title="NightSky"><h2>NightSky</h2></a>
-                   <p>Tens of thousands of satellites orbiting Earth will hamper astronomers efforts to study the Universe and spot dangerous asteroids.</p>
+                   {/* <p>Tens of thousands of satellites orbiting Earth will hamper astronomers efforts to study the Universe and spot dangerous asteroids.</p> */}
                 </div>
             </div>
 
